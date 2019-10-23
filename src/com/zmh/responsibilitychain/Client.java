554871491b6c986dd737e1b646700472a54c3df3
zhangmenghan <2423306508@qq.com>
2019-10-23 10:23:09 +0800

    04

--- src/com/zmh/responsibilitychain/Client.java
+++ src/com/zmh/responsibilitychain/Client.java
@@ -3,15 +3,17 @@ package com.zmh.responsibilitychain;
 public class Client {
 
 	public static void main(String[] args) {
-		//创建一个请求
-		PurchaseRequest purchaseRequest = new PurchaseRequest(1, 31000, 1);
+		//创建请求
+		PurchaseRequest purchaseRequest1 = new PurchaseRequest(1, 4500, 1);
+		PurchaseRequest purchaseRequest2 = new PurchaseRequest(1, 6700, 1);
+		PurchaseRequest purchaseRequest3 = new PurchaseRequest(1, 10001, 1);
+		PurchaseRequest purchaseRequest4 = new PurchaseRequest(1, 30001, 1);
 		
 		//创建相关的审批人
 		DepartmentApprover departmentApprover = new DepartmentApprover("张主任");
 		CollegeApprover collegeApprover = new CollegeApprover("李院长");
 		ViceSchoolMasterApprover viceSchoolMasterApprover = new ViceSchoolMasterApprover("王副校");
 		SchoolMasterApprover schoolMasterApprover = new SchoolMasterApprover("佟校长");
-	
 	
 		//需要将各个审批级别的下一个设置好 (处理人构成环形: )
 		departmentApprover.setApprover(collegeApprover);
@@ -21,8 +23,10 @@ public class Client {
 		
 		
 		
-		departmentApprover.processRequest(purchaseRequest);
-		viceSchoolMasterApprover.processRequest(purchaseRequest);
+		departmentApprover.processRequest(purchaseRequest1);
+		viceSchoolMasterApprover.processRequest(purchaseRequest2);
+		schoolMasterApprover.processRequest(purchaseRequest3);
+		collegeApprover.processRequest(purchaseRequest4);
 	}
 
 }
